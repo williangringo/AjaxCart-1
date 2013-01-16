e0300a8ced048e29de73cf62c37e9bfc94364840
Max Stepantsevich <st.maxik@gmail.com>
2013-01-16 08:05:32 +0200

    Update app/code/local/Ikantam/AjaxCart/Model/Frontend/Selector.php

--- app/code/local/Ikantam/AjaxCart/Model/Frontend/Selector.php
+++ app/code/local/Ikantam/AjaxCart/Model/Frontend/Selector.php
@@ -32,20 +32,16 @@ class Ikantam_AjaxCart_Model_Frontend_Selector
 		// header links 
 		$this->_selectors['toplinks'] = '.links';
 
-        $this->_selectors['cart'] = '.cart';
+        	$this->_selectors['cart'] = '.cart';
 		
 	}
 	
 	public function getSelector( $blockId )
 	{
-		foreach ($this->_selectors as $_blockId => $_selector){			
-			if( $_blockId === $blockId ){
-				return $_selector;
-			}
-		}
-		
-		return null;
-		
+		if( isset( $this->_selectors[$blockId] ) ){
+			return $this->_selectors[$blockId];
+		}		
+		return null;		
 	}
 	
 	public function getSelectors()
